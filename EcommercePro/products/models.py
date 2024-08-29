bce081b53abaf18878de1d64df9f1041a0253e67
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-29 11:56:30 +0300

    create products app

---
+++ EcommercePro/products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=5 , decimal_places=2)
+    image = models.ImageField(upload_to='photos/%y/%m/%d')
+    active = models.BooleanField(default=True)
